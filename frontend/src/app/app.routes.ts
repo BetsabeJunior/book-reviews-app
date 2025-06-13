8da913a15ed55ce1237437a2478659737250b23e
Betsabe Junior Hoyos Barrios <betsabehoyos@gmail.com>
2025-06-13 08:15:27 -0500

    fix(app.routes): Se agrega nuevas rutas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,14 +1,22 @@
 import { Routes } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard';
 import { LoginComponent } from './features/auth/login/login.component';
+import { RegisterComponent } from './features/auth/register/register.component';
+import { ForgotPasswordComponent } from './features/auth/forgot-password/forgot-password.component';
 import { HomeComponent } from './features/home/home.component';
 import { BookDetailComponent } from './features/book-detail/book-detail.component';
 import { ProfileComponent } from './features/profile/profile.component';
 
 export const routes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'book/:id', component: BookDetailComponent, canActivate: [AuthGuard] },
   { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
-  { path: '**', redirectTo: '' }
+
+  { path: '**', redirectTo: 'login' }
 ];
