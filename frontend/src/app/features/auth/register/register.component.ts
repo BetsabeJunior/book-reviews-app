6b659c78f3b81fc11a0be8b5f939494ba8985286
Betsabe Junior Hoyos Barrios <betsabehoyos@gmail.com>
2025-06-13 08:17:28 -0500

    feat(register.component): Se agrega componente para poder registrar al usuario

--- frontend/src/app/features/auth/register/register.component.ts
+++ frontend/src/app/features/auth/register/register.component.ts
@@ -1,11 +1,62 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormGroupName, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  imports: [ReactiveFormsModule, CommonModule, RouterModule],
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
+  loading = false;
+  errorMessage = '';
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.registerForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get name() {
+    return this.registerForm.get('name');
+  }
+
+  get email() {
+    return this.registerForm.get('email');
+  }
+
+  get password() {
+    return this.registerForm.get('password');
+  }
+
+  onSubmit(): void {
+    if (this.registerForm.invalid) {
+      this.registerForm.markAllAsTouched();
+      return;
+    }
+
+    this.loading = true;
+    this.errorMessage = '';
+    this.authService.register(this.registerForm.value).subscribe({
+      next: (response) => {
+        console.log('Registro exitoso:', response);
+        this.router.navigate(['/home']);
+      },
+      error: (err: any) => {
+        console.error('Error en el registro:', err);
+        this.errorMessage = err.error?.message || 'Registration failed.';
+        this.loading = false;
+      }
+    });
+
+  }
 }
