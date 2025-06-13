96d0540c07dd91f576b8fffedbedf2d025f37e5e
Betsabe Junior Hoyos Barrios <betsabehoyos@gmail.com>
2025-06-13 08:16:49 -0500

    feat(forgot-password.component): Se agrega diseño y logica del componente de olvidar contraseña

--- frontend/src/app/features/auth/forgot-password/forgot-password.component.ts
+++ frontend/src/app/features/auth/forgot-password/forgot-password.component.ts
@@ -1,11 +1,53 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../../core/services/auth.service';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [],
   templateUrl: './forgot-password.component.html',
-  styleUrl: './forgot-password.component.css'
+  imports: [ReactiveFormsModule, CommonModule, RouterModule],
 })
 export class ForgotPasswordComponent {
+  forgotPasswordForm: FormGroup;
+  loading = false;
+  successMessage = '';
+  errorMessage = '';
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService, 
+   private router: Router
+  ) {
+    this.forgotPasswordForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  get email() {
+    return this.forgotPasswordForm.get('email');
+  }
+
+  onSubmit(): void {
+    if (this.forgotPasswordForm.invalid) {
+      this.forgotPasswordForm.markAllAsTouched();
+      return;
+    }
+
+    this.loading = true;
+    this.errorMessage = '';
+    this.successMessage = '';
+
+    this.authService.forgotPassword(this.email?.value).subscribe({
+      next: (res : any) => {
+        this.successMessage = res.message || 'Email sent successfully.';
+        this.loading = false;
+      },
+      error: (err: any) => {
+        this.errorMessage = err.error?.message || 'Error sending email.';
+        this.loading = false;
+      }
+    });
+  }
 }
